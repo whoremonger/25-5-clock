814d92a7308a74e6350b6c9e04386bc0ba00e8dc
Jarrad L <jlovelace2500k@gmail.com>
2024-08-13 21:27:52 -0700

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://whoremonger.github.io/25--5--clock/",
+  base: "https://whoremonger.github.io/25-5-clock/",
 })
